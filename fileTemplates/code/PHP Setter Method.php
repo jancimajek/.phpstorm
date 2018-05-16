7cfc2665f0ded6e32ef58b5f2469cd4f9a8db3c0
Jan Majek <jan.majek@ft.com>
2018-05-16 16:57:22 +0100

    PS-2017.2 <jan.majek@FT-MW4298 Create baseRefactoring.xml, PHP Class.php, IntelliLang.xml, ide.general.xml, textmate.xml, project.default.xml, diff.xml, git.xml, other.xml, Default.xml, file.template.settings.xml, ignore.xml, xpath.xml, find.xml, ui.lnf.xml, filetypes.xml, php.xml, code.style.schemes.xml, plugin_ui.xml, watcherDefaultTasks.xml, colors.scheme.xml, External Tools.xml, notifications.xml, path.macros.xml, databaseDrivers.xml, github_settings.xml, customization.xml, Blogs tools.xml, Default.xml, Build.xml, laf.xml, Default _1_.xml, Mac OS X 10_5_ copy.xml, web-browsers.xml, proxy.settings.xml, vcs.xml, editor.codeinsight.xml, PHP Setter Method.php, databaseSettings.xml, keymap.xml, editor.xml, usageView.xml, applicationLibraries.xml, markdown_navigator.shared.xml, webServers.xml, debugger.xml, AWS CloudFormation _JSON_.xml

---
+++ fileTemplates/code/PHP Setter Method.php
@@ -0,0 +1,14 @@
+/**
+ * @param ${TYPE_HINT} $${PARAM_NAME}
+ * @return ${CLASS_NAME}
+ */
+public ${STATIC} function set${NAME}(${TYPE_HINT} $${PARAM_NAME})
+{
+#if (${STATIC} == "static")
+    self::$${FIELD_NAME} = $${PARAM_NAME};
+#else
+    $this->${FIELD_NAME} = $${PARAM_NAME};
+#end
+
+    return $this;
+}
